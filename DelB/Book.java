40975ec3ae072d79dd84c15b4de1c5193716d4a5
Douglas1410 <dpham@kth.se>
2016-09-24 21:22:03 +0200

    update

--- DelB/Book.java
+++ DelB/Book.java
@@ -1,10 +1,6 @@
 package DelB;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,40 +19,40 @@ public class Book implements Comparable<Book>, Serializable{
 		this.edition=edition;
 		this.price=price;
 		authors = new ArrayList<Author>();
-		
-		
-		
+
+
+
 	}
 	public Book(){
-		
+
 	}
 	public void addAuthor(String author){
 		authors.add(new Author(author));
 	}
-	
+
 	public String getTitle() {
 		return title;
 	}
 	public String getIsbn() {
 		return isbn;
 	}
-	
-	
+
+
 	public ArrayList<Author> getAuthors(){
 
-		
+
 		return (ArrayList<Author>) authors.clone();
-		
+
 	}
 	@Override
 	public int compareTo(Book o) {
 		String title= this.title;
 		String otherBook = o.title;
 		int res = title.compareTo(otherBook);
-		
+
 		return res;
 	}
-	
+
 	public boolean equals(Object other){
 		if(other instanceof Book){
 			Book b= (Book) other;
@@ -71,50 +67,14 @@ public class Book implements Comparable<Book>, Serializable{
 		String authorsofBook="";
 		for (Author a : this.authors) {
 				authorsofBook+=a.getAuthor();
-			
+
 		}
 		info = " "+ title +" "+ edition +" "+ isbn +" "+ price +" "+authorsofBook+"\n";
 		return info;
 	}
-	
-	
-	
-	
-	public static void main(String[] args) throws IOException {
-		Book b = new Book("12345","Java",1,200);
-		b.addAuthor("Douglas");
-		b.addAuthor("Danny");
-		FileOutputStream bookFileOut= null;
-		try {
-			 bookFileOut = new FileOutputStream("/tmp/Book.ser");
-			ObjectOutputStream out = new ObjectOutputStream(bookFileOut);
-			out.writeObject(b);
-			
-			out.close();
-		} catch (Exception e) {
-			e.printStackTrace();;
-		} finally {
-			bookFileOut.close();
-		}
-			
-		
-		Book Ob = null;
-		FileInputStream bookFileIn = null;
-		try {
-			 bookFileIn = new FileInputStream("/tmp/Book.ser");
-			ObjectInputStream in = new ObjectInputStream(bookFileIn);
-			Ob= (Book) in.readObject();
-			in.close();
-			
-		} catch (Exception i) {
-			System.out.println("Book not found");
-			
-		}finally {
-				bookFileIn.close();
 
-		}
-		
-		System.out.println(Ob.toString());
-		
-	}
+
+
+
+
 }
--- DelB/CollectionOfBooks.java
+++ DelB/CollectionOfBooks.java
@@ -3,7 +3,7 @@ package DelB;
 import java.util.ArrayList;
 import java.util.Collections;
 
-	
+
 public class CollectionOfBooks {
 	private ArrayList<Book> bookCollections;
 
@@ -25,12 +25,12 @@ public class CollectionOfBooks {
 		ArrayList<Book>CollectionByTitle =new ArrayList<Book>();
 		CollectionByTitle.clear();
 		for (Book b : bookCollections) {
-			
+
 			if(b.getTitle().toUpperCase().contains(Title.toUpperCase())){
 				CollectionByTitle.add(b);
 			}
 			Collections.sort(CollectionByTitle);
-			
+
 		}
 		return CollectionByTitle;
 	}
@@ -43,7 +43,7 @@ public class CollectionOfBooks {
 				if (a.getAuthor().toUpperCase().contains(author.toUpperCase())) {
 					CollectionByAuthor.add(book);
 				}
-				
+
 			}
 			Collections.sort(CollectionByAuthor);
 		}
@@ -52,7 +52,7 @@ public class CollectionOfBooks {
 	public ArrayList<Book> getBookByIsbn(String isbn){
 		ArrayList<Book>CollectionByIsbn = new ArrayList<Book>();
 		CollectionByIsbn.clear();
-		
+
 			for (Book book : bookCollections) {
 				if(book.getIsbn().toUpperCase().contains(isbn.toUpperCase())){
 					CollectionByIsbn.add(book);
@@ -66,30 +66,6 @@ public class CollectionOfBooks {
 		String info = bookCollections.toString();
 		return info;
 	}
-	public static void main(String[] args) {
-		CollectionOfBooks c = new CollectionOfBooks();
-		Book tmp = null;
-		tmp = new Book("1234", "Foretagsekonomi", 1, 200);
-		tmp.addAuthor("Douglas");
-		tmp.addAuthor("Danny");
-		c.addBook(tmp);
-		tmp = new Book("1235", "Foretag och Samhallet", 1, 500);
-		tmp.addAuthor("Deck");
-		tmp.addAuthor("Donal");
-		c.addBook(tmp);
-		tmp=new Book("2312","Finasering" , 2, 700);
-		tmp.addAuthor("Douglas");
-		tmp.addAuthor("Martin");
-		c.addBook(tmp);
-		tmp =new Book("2345", "Animal Planet", 4, 200);
-		tmp.addAuthor("Douglas");
-		tmp.addAuthor("Fredrik");
-		c.addBook(tmp);
-		
-		System.out.println(c.toString());
-		System.out.println(c.getBookByAuthor("Martin"));
-		System.out.println(c.getBookByIsbn("1234").toString());
-		System.out.println(c.getBooksByTitle("f").toString());
-	}
+
 
 }
