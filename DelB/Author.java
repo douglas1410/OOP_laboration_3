7ae2d95c37e6dd5aa560b6e3dc3b999d3f53c24d
vge1 <vedwall@kth.se>
2016-09-25 10:40:32 +0200

    Update Author.java

--- DelB/Author.java
+++ DelB/Author.java
@@ -1,17 +1,26 @@
 package DelB;
 
 import java.io.Serializable;
-
+/**
+*
+*/
 public class Author implements Serializable {
+	
 	private String name;
 	
+	/**
+	*Constructor
+	*@param name Name of the author.
+	*/
 	public Author(String name){
 		this.name = name;
 	}
 	
+	/**
+	*Get name of the author.
+	*@return Name of the author.
+	*/
 	public String getName(){
 		return name;
-	}
-	
 
 }
