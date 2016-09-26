54faa9dcc4694820cb528bed1a8f4c4b934da259
douglas1410 <dpham@kth.se>
2016-09-26 10:35:54 +0200

    Update Retangle.java

--- DelA2/Retangle.java
+++ DelA2/Retangle.java
@@ -2,7 +2,9 @@ package DelA;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
-
+/**
+*An instace of Retangle with height 50 and width = 100
+*/
 public class Retangle extends FillableShape {
 	private double height;
 	private double width;
@@ -11,25 +13,42 @@ public class Retangle extends FillableShape {
 		height= 50.0;
 		width = 100.0;
 	}
-	
+	/**
+	*Constructor without arguments initiate height = 50, width =100
+	*
+	*/
 	public Retangle(double x,double y,Color color,double height, double width){
 		super(x, y, color);
 		this.height=height;
 		this.width=width;
 	}
+	/**
+	* return rectangles width
+	*/
 	public double getWidth(){
 		return width;
 	}
+	/**
+	*return retangles height
+	*/
 	public double getHeight() {
 		return height;
 	}
+	/**
+	* set a width value to the object rectangle
+	*/
 	public void setWidth(double width) {
 		this.width = width;
 	}
+	/**
+	* set a Height value to the object rectangle
+	*/
 	public void setHeight(double height) {
 		this.height = height;
 	}
-	
+	/**
+	* the graphics context
+	*/
 	@Override
 	public void paint(GraphicsContext gc) {
 		gc.setFill(getColor());
@@ -43,6 +62,9 @@ public class Retangle extends FillableShape {
 		}
 		
 	}
+	/**
+	* change direction of object retangle if it collides with the box's constrains
+	*/
 	@Override
 	public void constrain(double boxX, double boxY, double boxWidth, double boxHeight) {
 		if (getX()<boxX) {
