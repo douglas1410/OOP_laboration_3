7fa7002be3eae1e46995d1c64b1ae3a3bc2a578d
douglas1410 <dpham@kth.se>
2016-09-26 10:43:03 +0200

    Update Circle.java

--- DelA2/Circle.java
+++ DelA2/Circle.java
@@ -2,27 +2,43 @@ package DelA;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
-
+/**
+* the class defines a circle that can move and change direction
+*/
 public class Circle extends FillableShape {
 	private double diameter;
+	/**
+	* constuctor without arguments
+	*/
 	public Circle() {
 		super();
 		diameter =1;
 		
 	}
+	/**
+	* constructor with a start point and diameter in arguments
+	*/
 	public Circle(double x ,double y ,Color color , double diameter){
 		super(x, y, color);
 		this.diameter= diameter;
 		
 	}
+	/**
+	* return diameter
+	*/
 	
 	 public double getDiameter() {
 		return diameter;
 	}
+	/**
+	* set a new diameter value to circle object
+	*/
 	public void setDiameter(double diameter){
 		this.diameter=diameter;
 	}
-	
+	/**
+	* change direction of circle object if it collides with the box's constranins
+	*/
 	 public void constrain(double boxX, double boxY, double boxWidth, double boxHeight) {
 		if (getX()<boxX) {
 			setVelocity(Math.abs(getDx()), getDy());
@@ -40,7 +56,9 @@ public class Circle extends FillableShape {
 		
 
 	}
-	
+	/**
+	* the graphics context 
+	*/
 	@Override
 	public void paint(GraphicsContext gc) {
 		gc.setFill(getColor());
